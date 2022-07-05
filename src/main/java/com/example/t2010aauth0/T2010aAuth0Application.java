021385f26ba30b851364a305313d8ee0c669f883
Nhật Nguyễn Tiến <kaotuong113@gmail.com>
2022-07-05 15:21:21 +0700

    push

---
+++ src/main/java/com/example/t2010aauth0/T2010aAuth0Application.java
@@ -0,0 +1,13 @@
+package com.example.t2010aauth0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2010aAuth0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T2010aAuth0Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/t2010aauth0/T2010aAuth0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.t2010aauth0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2010aAuth0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
